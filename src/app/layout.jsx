8290c27c36b9c4a7bec8ee60e6e76a8ed8ad6a85
SAPPATH <smileflaoscorp@gmail.com>
2025-08-18 17:55:30 +0900

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,21 +1,16 @@
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'SAPPATH Cafe',
   description: 'Welcome to SAPPATH Cafe',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={inter.className}>{children}</body>
     </html>
   )
