537c49b64a2ce31f32ef1b6405df88a66ee302f4
SAPPATH <smileflaoscorp@gmail.com>
2025-08-18 18:23:28 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/app/**/*.{js,jsx}",
+    "./src/components/**/*.{js,jsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
