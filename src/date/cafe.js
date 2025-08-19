92586a7816032803bc05af4abc6085a7156893e3
SAPPATH <smileflaoscorp@gmail.com>
2025-08-19 18:45:56 +0900

    Create cafe.js

---
+++ src/date/cafe.js
@@ -0,0 +1,21 @@
+// src/data/cafe.js
+export const copy = {
+  heroTitle: "EFLAO CAFE",
+  heroLead: "ラオスの香りを日常に。フェアトレードの豆と、やさしいおやつ。",
+};
+
+export const links = {
+  instagram: "https://instagram.com/eflao_cafe",
+  x: "https://x.com/eflao_cafe",
+  maps: "https://maps.google.com/?q=EFLAO+CAFE",
+  reserve: "#contact",
+};
+
+export const gallery = [
+  "/images/gallery-1.jpg",
+  "/images/gallery-2.jpg",
+  "/images/gallery-3.jpg",
+  "/images/gallery-4.jpg",
+  "/images/gallery-5.jpg",
+  "/images/gallery-6.jpg",
+];
